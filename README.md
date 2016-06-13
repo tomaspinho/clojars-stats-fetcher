c2d2c3441f43dc498f8ae9e7f40131b1c00d996e
Tomás Pinho <me@tomaspinho.com>
2016-06-13 21:45:14 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,14 @@
+# clojars-stats-fetcher
+
+A Clojure program that fetches packages' statistics from Clojars and that writes them to a `.csv` file for analysis.
+
+## Usage
+
+    # run
+    lein run [FILE] # defaults to out.csv
+
+## License
+
+Copyright © 2016 Tomás Pinho
+
+Distributed under the MIT License.
---
+++ src/clojars_stats_fetcher/core.clj
@@ -0,0 +1,57 @@
+(ns clojars-stats-fetcher.core
+  (:require [org.httpkit.client :as http]
+            [clojure.string :as str]
+            [clojure.data.json :as json]))
+
+(defn download-clj-depedencies
+  []
+  (let [{:keys [body] :as resp} @(http/get "https://clojars.org/repo/all-jars.clj")]
+    body))
+
+(defn deps-to-pkgs-lst
+  "This function grabs a raw clj deps list in raw format and parses it, removing
+  duplicates and returning a list of unique packages."
+  [deps]
+  (distinct (map #((re-matches #"^\[([a-zA-Z0-9/.\-_]+) .*$" %) 1) (str/split deps #"\n"))))
+
+(defn mk-fetch-pkg-data
+  [print-each num-pkgs]
+  (fn fetch-pkg-data
+    [i pkg]
+    (if (= (rem i print-each) 0)
+        (println "Downloaded data for" i "/" num-pkgs))
+    (let [uri (str "https://clojars.org/api/artifacts/" (str/lower-case pkg))
+         {:keys [body error]} @(http/get uri)]
+         (if error
+             (fetch-pkg-data i pkg)
+             body))
+    ))
+
+(defn -main [& args]
+  (let [filename (if (nil? (first args)) "out.csv" (first args))
+        package-list (deps-to-pkgs-lst (download-clj-depedencies))]
+
+    (spit filename "name,homepage,description,downloads\n")
+    (println "Downloaded list, processing" (count package-list) "elements.")
+
+    (let [responses (map-indexed
+      (mk-fetch-pkg-data 100 (count package-list))
+      package-list)]
+
+      (doall (for [r responses]
+        (do
+        (let [j (if (not= r "")
+                    (json/read-str r)
+                    false)]
+        (if j
+            (spit filename (str (if (not= (j "jar_name") (j "group_name"))
+                                          (j "jar_name"))
+                                          (str (j "group_name") "/" (j "jar_name"))
+                            ","
+                            (j "homepage")
+                            ","
+                            (j "description")
+                            ","
+                            (j "downloads")
+                            "\n") :append true))))))
+)))
